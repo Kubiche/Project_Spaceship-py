d6314dd850c98aad586b49ec5bce9cbae639c307
Kubiche <juanquy25@gmail.com>
2023-02-06 17:01:35 -0500

    Rocket class added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+import time
+import serial
 import pygame
 
 
@@ -15,17 +18,28 @@ clock = pygame.time.Clock()
 # Initialize the joysticks
 pygame.joystick.init()
 
+
+# Sound instances
 mainenginesound = pygame.mixer.Sound("/Sounds/main_engines.mp3")
 
 
 
 class Rocket:
-    def __init__(self,fuel=100, health=100, acceleration=0, speed=0, heading=0):
+    def __init__(self,fuel=100, health=100, acceleration=0, velocity=0, angle=90, altitude = 0):
         self.fuel = fuel
         self.health = health
         self.acceleration = acceleration
-        self.speed = speed
-        self.heading = heading
+        self.velocity = velocity
+        self.angle = angle  # Referenced from the positive side of X in a 2D representation.
+        self.altitude = altitude
+        self.last_update = time.time()
+    
+    def update(self):        
+        time_elapsed = time.time() - self.last_update
+        self.velocity = self.acceleration / time_elapsed
+        self.altitude = self.altitude + ((self.velocity*math.sin(math.radians(self.angle))) * time_elapsed)
+
+        self.last_update = time.time()  # Update last to record the time the update was finished
 
 
 while True:
