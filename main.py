94c79fb7161d4d987e7437bfd169e83b3fa9ce8c
Kubiche <juanquy25@gmail.com>
2023-02-08 12:35:12 -0500

    audio initial test

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ pygame.joystick.init()
 # Get count of joysticks
 joystick_count = pygame.joystick.get_count()
 
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
+#joystick = pygame.joystick.Joystick(0)
+#joystick.init()
 
 
 # Sound instances
 mainenginesound = pygame.mixer.Sound("C:\\Users\\jalvarez\\My Drive\\CODE\\Project_Spacecraft_py\\Project_Spaceship_Rpi\\Sounds\\main_engines.mp3")
+
+pygame.display.set_mode((800, 600))
 
 
 
@@ -34,10 +36,19 @@ r1 = Rocket()
 # ser = serial.Serial("/dev/ttyAMA0", 115200)
 # serialFromArduino.flush()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEWHEEL:
-            mainenginesound.play()
+on = True
+
+while on:    
+    for event in pygame.event.get():        
+        if event.type == pygame.KEYDOWN:
+            print("Key press detected")
+            if event.key == pygame.K_ESCAPE:
+                print("Escape key pressed")
+                on = False
+            if event.key == pygame.K_k:
+                print("k key pressed")
+                mainenginesound.play(-1)
+
     
     
     r1.update  # Updates the rocket properties like velocity, altitude etc.
